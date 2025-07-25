23782db80cb0dded95a1a1e21945159ef9a356d5
AnaisBresle <anais.bresle@gmail.com>
2025-07-25 15:18:33 +0100

    updated script so meaasge is in p tag for styling

--- public/script.js
+++ public/script.js
@@ -52,7 +52,8 @@ function renderNote(note) {  // build html for one note
   const noteEl = document.createElement('div');
   noteEl.classList.add('note');
   noteEl.innerHTML = `
-    <h3>${note.title}</h3> - ${note.message}
+    <h3>${note.title}</h3>
+    <p>${note.message}</p>
      <button onclick="getNoteToEdit('${note.id}')">Edit</button> 
     <button class="deletebtn" onclick="deleteNote('${note.id}')">Delete</button>
     `;
