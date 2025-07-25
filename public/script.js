0f751eef4c317139496b556d997ab557f44925ce
AnaisBresle <anais.bresle@gmail.com>
2025-07-25 11:33:00 +0100

    getting the notes

--- public/script.js
+++ public/script.js
@@ -1,3 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
   fetchNotes();
+})
+
+
+function fetchNotes() {
+  fetch('/notes') /// GET reuqest 
+    .then(res => res.json()) // notes in JSON format
+    .then(data => {
+      const notesContainer = document.getElementById('notesContainer'); /// place where we are displaying all existig notes. 
+      notesContainer.innerHTML = ''; // clear existing view in container (so it gets replaced by the updated list of all notes)
+      data.forEach(renderNote); // rendering notes for HMTL
+    })
+    .catch(err => console.error('Error loading notes:', err));
 }
+
+function renderNote () {}
